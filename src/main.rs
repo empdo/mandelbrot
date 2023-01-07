0a18cd2430878564d5979e20e3ed4165e9077f26
empdo <emil.essung@live.se>
2023-01-07 23:13:19 +0100

    coooooolors

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::WindowCanvas;
+use std::collections::HashMap;
 use std::time::Duration;
+
+use stopwatch::Stopwatch;
 
 use coolor;
 
@@ -13,21 +16,24 @@ pub fn draw_mandel(
     canvas: &mut WindowCanvas,
     width: u32,
     height: u32,
-    x_offset: f32,
-    y_offset: f32,
+    x_offset: f64,
+    y_offset: f64,
     max_it: i32,
-    value: f32,
+    value: f64,
 ) {
+//    let mut sw = Stopwatch::start_new();
+
+
     for x in 0..width {
         for y in 0..height {
-            let mut zx = (((x as f32) / (width as f32)) * value) - x_offset;
-            let mut zy = (((y as f32) / (height as f32)) * value) - y_offset;
+            let mut zx = (((x as f64) / (width as f64)) * value) - x_offset;
+            let mut zy = (((y as f64) / (height as f64)) * value) - y_offset;
 
             let cx = zx;
             let cy = zy;
 
-            let mut n = 0;
-            while n < max_it {
+            let mut n: f64 = 0.0;
+            while n < max_it as f64 {
                 let _zx = zx * zx - zy * zy + cx;
                 zy = 2.0 * zx * zy + cy;
                 zx = _zx;
@@ -35,17 +41,20 @@ pub fn draw_mandel(
                 if (zx * zx + zy * zy) > 4.0 {
                     break;
                 }
-                n += 1;
+                n += 1.0;
             }
 
-            let n = n as f32 / max_it as f32;
+            n = n / max_it as f64;
+            let mut color: coolor::Rgb;
 
-            let color = coolor::Hsl::new(163.0, 1.0, n).to_rgb();
-
-            canvas.set_draw_color(Color::RGB(color.r , color.g , color.b ));
+            
+            color = coolor::Hsl::new(286.0, 1.0, n as f32).to_rgb();
+            canvas.set_draw_color(Color::RGB(color.r, color.g, color.b));
             canvas.fill_rect(Rect::new(x as i32, y as i32, 1, 1));
         }
     }
+//    println!("Took {}ms to draw shape", sw.elapsed_ms());
+//    sw.stop();
 }
 
 pub fn main() {
@@ -139,7 +148,7 @@ pub fn main() {
             x_offset,
             y_offset,
             max_it,
-            value,
+            value as f64,
         );
 
         canvas.present();
